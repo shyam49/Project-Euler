22d096fcdfbec8e3cd26e2901d226bf36927339d
KamleshGupta123 <gkamal625@gmail.com>
2017-10-26 12:24:02 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -1,6 +1,6 @@
 a=[1,2]
-for i in range(2,100000):
-	if a[i-1]+a[i-2]<4000000:
+for i in range(2,10000):
+	if a[i-1]+a[i-2]<400000:
 		a.append(a[i-1]+a[i-2])
 	else:
 		break
@@ -8,4 +8,5 @@ sum=0
 for i in range(0,len(a)):
 	if a[i]%2==0:
 		sum+=a[i]
+		
 print sum
