4136a27982144d86950145ddac014e9c8f14263a
Sai Akhil Aloor <saiakhil.aloor7@gmail.com>
2017-10-26 11:51:14 +0530

    Update ques7.py

--- ques7.py
+++ ques7.py
@@ -1,8 +1,8 @@
 def primes(n):
-    sieve = [True] * n
+    sieve = [1] * n
     for i in xrange(3,int(n**0.5)+1,2):
         if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+            sieve[i*i::2*i]=[0]*((n-i*i-1)/(2*i)+1)
     return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
 print primes(1000000)[10000]
